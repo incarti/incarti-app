44603e16acd3529a208239e9f3b6ea4cf4ddd5a9
Antony <allinto@icloud.com>
2021-03-13 17:48:35 +1300

    commit

--- src/js/views/Login.js
+++ src/js/views/Login.js
@@ -84,10 +84,15 @@ class Login extends Component {
       <div id="login-content">
         <form id="login-form" autocomplete="off" onSubmit=${e => onLoginFormSubmit(e)}>
           <div id="create-account">
-            <img style="width: 86px" src="img/android-chrome-192x192.png" alt="Iris"/>
-            <h1>Iris</h1>
-            <input onInput=${e => onNameChange(e)} autocomplete="off" autocorrect="off" autocapitalize="sentences" spellcheck="off" id="login-form-name" type="text" name="name" placeholder="${t('whats_your_name')}"/>
-            <p><button id="sign-up" type="submit">${t('new_user_go')}</button></p>
+            <h1 style="color: black; font-family: arialBlack;">INCARTI</h1>
+            <div style="display: flex">
+              <input class="logIn" onInput=${e => onNameChange(e)} autocomplete="off" autocorrect="off" autocapitalize="sentences" spellcheck="off" id="login-form-name" type="text" name="name" placeholder="${t('whats_your_name')}"/>
+              <p><button id="sign-up" type="submit">
+                <svg class="svg-icon" viewBox="0 0 20 20">
+                  <path fill="none" d="M11.611,10.049l-4.76-4.873c-0.303-0.31-0.297-0.804,0.012-1.105c0.309-0.304,0.803-0.293,1.105,0.012l5.306,5.433c0.304,0.31,0.296,0.805-0.012,1.105L7.83,15.928c-0.152,0.148-0.35,0.223-0.547,0.223c-0.203,0-0.406-0.08-0.559-0.236c-0.303-0.309-0.295-0.803,0.012-1.104L11.611,10.049z"></path>
+                </svg>
+              </button></p>
+            </div>
             <br/>
             <p><a href="#" id="show-existing-account-login" onClick=${e => showSwitchAccount(e)}>${t('already_have_an_account')}</a></p>
             <p>
